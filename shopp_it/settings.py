e7feac4160742ed4ca9d6754f018a93bc9b9cad9
= <=>
2025-10-25 08:01:43 -0700

    Fix media file serving

--- shopp_it/settings.py
+++ shopp_it/settings.py
@@ -164,6 +164,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/img/'
 MEDIA_ROOT = BASE_DIR / "media"
+WHITENOISE_ROOT = MEDIA_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
